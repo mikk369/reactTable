d7c83afde777627d27f28f02d925306cc1f41da7
Mikk <jogdial25@gmail.com>
2023-11-24 14:29:14 +0200

    table sorting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import Switch from '@mui/material/Switch';
-// import { useEffect, useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 // import axios from 'axios';
+
+const data = [
+  {"date": "2023-01-01", "food_name": "Pizza", "price": 15.99, "hold_price": 5.00, "user_id": 1},
+  {"date": "2023-01-01", "food_name": "Milk", "price": 0.99, "hold_price": 5.00, "user_id": 1},
+  {"date": "2023-01-02", "food_name": "Burger", "price": 5.99, "hold_price": 5.00, "user_id": 1},
+  {"date": "2023-01-02", "food_name": "Salat", "price": 1.99, "hold_price": 5.00, "user_id": 1},
+  {"date": "2023-01-03", "food_name": "Bread", "price": 2.50, "hold_price": 5.00, "user_id": 1},
+  {"date": "2023-01-02", "food_name": "Burger", "price": 9.99, "hold_price": 2.50, "user_id": 2},
+  {"date": "2023-01-02", "food_name": "Bread", "price": 1.99, "hold_price": 2.50, "user_id": 2},
+  {"date": "2023-01-03", "food_name": "Juice", "price": 0.99, "hold_price": 2.50, "user_id": 2},
+  {"date": "2023-01-03", "food_name": "Steak", "price": 9.99, "hold_price": 2.50, "user_id": 2},
+  {"date": "2023-01-03", "food_name": "Salad", "price": 0.99, "hold_price": 2.50, "user_id": 2},
+  {"date": "2023-01-03", "food_name": "Pasta", "price": 12.50, "hold_price": 3.00, "user_id": 3},
+  {"date": "2023-01-03", "food_name": "Bread", "price": 1.50, "hold_price": 3.00, "user_id": 3},
+  {"date": "2023-01-03", "food_name": "Milk", "price": 0.50, "hold_price": 3.00, "user_id": 3},
+  {"date": "2023-01-03", "food_name": "Steak", "price": 12.50, "hold_price": 3.00, "user_id": 3},
+  {"date": "2023-01-04", "food_name": "Sushi", "price": 20.99, "hold_price": 5.50, "user_id": 4},
+  {"date": "2023-01-04", "food_name": "Bread", "price": 0.99, "hold_price": 5.50, "user_id": 4},
+  {"date": "2023-01-04", "food_name": "Juice", "price": 0.99, "hold_price": 5.50, "user_id": 4},
+  {"date": "2023-01-04", "food_name": "Steak", "price": 20.99, "hold_price": 5.50, "user_id": 4},
+  {"date": "2023-01-05", "food_name": "Salad", "price": 8.99, "hold_price": 1.50, "user_id": 5},
+  {"date": "2023-01-06", "food_name": "Steak", "price": 24.99, "hold_price": 6.00, "user_id": 6},
+  {"date": "2023-01-07", "food_name": "Chicken Wings", "price": 14.50, "hold_price": 4.00, "user_id": 7},
+  {"date": "2023-01-08", "food_name": "Tacos", "price": 10.99, "hold_price": 2.00, "user_id": 8},
+  {"date": "2023-01-09", "food_name": "Soup", "price": 7.99, "hold_price": 1.00, "user_id": 9},
+  {"date": "2023-01-10", "food_name": "Sandwich", "price": 6.50, "hold_price": 1.50, "user_id": 10}
+]
 
 function App() {
   return (
@@ -10,54 +38,70 @@ function App() {
   );
 }
 
-const data = [
-  { "date": "2023-01-01", "food_name": "Pizza", "price": 15.99, "hold_price": 2.00,       "user_id": 1 },
-      { "date": "2023-02-01", "food_name": "Milk", "price": 0.99, "hold_price": 0.20, "user_id": 1 },
-      { "date": "2023-03-01", "food_name": "Burger", "price": 5.99, "hold_price": 2.00, "user_id": 1 },
-      { "date": "2023-04-01", "food_name": "Salat", "price": 1.99, "hold_price": 1.00, "user_id": 1 },
-      { "date": "2023-05-01", "food_name": "Bread", "price": 0.50, "hold_price": 0.22, "user_id": 1 }
-]
-
 function Table() {
-  // const [tableData, setTableData] = useState([]);
+  const [loggedInUserId] = useState(2);
+  const [showToitColumn, setShowToitColumn] = useState(true);
 
-  // useEffect(() => {
-  //   // Fetch data from your API/data.json
-  //   fetch('../API/data.json') // Adjust the path accordingly
-  //     .then((response) => response.json())
-  //     .then((data) => setTableData(data.orders)) // Access the 'orders' property
-  //     .catch((error) => console.error('Error fetching data:', error));
-  // }, []);
+  const filteredData = data.filter(item => item.user_id === loggedInUserId)
+
+  const dates = [...new Set(filteredData.map((item) => item.date))];
 
   return (
     <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '20px' }}>
       <thead>
         <tr>
-          <td colSpan="3" >
+          <td colSpan="4">
             <div style={tableViewHeader}>
-            <button><Switch  defaultChecked /></button><h2>Söökla kinnipidamine</h2><icon>X</icon>
+              <button onClick={() => setShowToitColumn(!showToitColumn)}>
+                <Switch defaultChecked />
+              </button>
+              <h2>Söökla kinnipidamine</h2>
+              <icon>X</icon>
             </div>
           </td>
         </tr>
         <tr>
           <th style={tableHeaderStyle}>Kuupäev</th>
+          {showToitColumn && <th style={tableHeaderStyle}>Toit</th>}
           <th style={tableHeaderStyle}>Summa</th>
           <th style={tableHeaderStyle}>Kinnipeetav</th>
         </tr>
       </thead>
       <tbody>
-        {data.map((row, index) => (
-          <tr key={index} style={(index % 2 === 0) ? evenRowStyle : oddRowStyle}>
-            <td style={tableCellStyle}>{row.date}</td>
-            <td style={tableCellStyle}>{row.price}</td>
-            <td style={tableCellStyle}>{row.hold_price}</td>
-          </tr>
-        ))}
+        {dates.map((date, index) => {
+          const dateItems = filteredData.filter((item) => item.date === date);
+          const total = dateItems.reduce((acc, curr) => acc + curr.price, 0);
+          const holdPrice = total / 2;
+
+          return (
+            <React.Fragment key={index}>
+              <tr>
+                <td style={tableCellStyle}>{date}</td>
+                <td style={tableCellStyle}></td>
+                {showToitColumn && <td style={Numeric}>{total}</td>}
+              </tr>
+              {dateItems.map((row, rowIndex) => (
+                <tr key={`${index}-${rowIndex}`} style={(rowIndex % 2 === 0) ? evenRowStyle : oddRowStyle}>
+                  <td style={tableCellStyle}></td>
+                  {showToitColumn && <td style={tableCellStyle}>{row.food_name}</td>}
+                  {showToitColumn ? <td style={tableCellStyle}>{row.price}</td> : <td style={tableCellStyle}>{total}</td>}
+                  <td style={tableCellStyle}>{holdPrice}</td>
+                </tr>
+              ))}
+            </React.Fragment>
+          );
+        })}
       </tbody>
     </table>
   );
 }
+const Numeric = {
+  fontWeight: 'bold',
+  padding: '10px',
+  borderBottom: '1px solid #ddd',
+  textAlign: 'center'
 
+}
 const tableViewHeader = {
   backgroundColor: '#d6d2d2',
   width: '100%',
